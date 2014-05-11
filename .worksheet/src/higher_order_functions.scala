bb3f98d0ab1c49f728099085e043aa4e85a8a04c
Mateus L Santos <mateus.lucios@gmail.com>
2014-05-11 20:46:13 -0300

    higher order functions. Done.

---
+++ .worksheet/src/higher_order_functions.scala
@@ -0,0 +1,3 @@
+object higher_order_functions {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(75); 
+  println("Welcome to the Scala worksheet")}
+}
---
+++ worksheets/higher_order_functions.sc
@@ -0,0 +1,3 @@
+object higher_order_functions {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+}
